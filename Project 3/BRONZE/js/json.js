2cf853540bbac7ccfffa278dc9ee4c0f26f5f43f
JTilley24 <justin.tilley@me.com>
2012-11-08 20:29:16 -0500

    Project 3

---
+++ Project 3/BRONZE/js/json.js
@@ -0,0 +1,185 @@
+//Justin Tilley
+//Project 1
+//MIU Term 1211
+
+var json = {
+	"repair1": {
+		"region": ["Region:", "Domestic"],
+		"make": ["Make:", "Chevy"],
+		"model": ["Model:", "Blazer"],
+		"year": ["Year:", "1996"],
+		"group": ["Part:", "Chassis"],
+		"date": ["Date for repair:", "2012-10-28"],
+		"comments": ["Description:", "ball-joints"]
+	},
+	"repair2": {
+		"region": ["Region:", "Asian"],
+		"make": ["Make:", "Honda"],
+		"model": ["Model:", "Civic"],
+		"year": ["Year:", "2001"],
+		"group": ["Part:", "Drivetrain"],
+		"date": ["Date for repair:", "2012-11-17"],
+		"comments": ["Description:", "replace transmission"]
+	},
+	"repair3": {
+		"region": ["Region:", "Domestic"],
+		"make": ["Make:", "Dodge"],
+		"model": ["Model:", "Ram 1500"],
+		"year": ["Year:", "2000"],
+		"group": ["Part:", "Body"],
+		"date": ["Date for repair:", "2012-11-13"],
+		"comments": ["Description:", "rear passenger wheel well rust"]
+	},
+	"repair4": {
+		"region": ["Region:", "Asian"],
+		"make": ["Make:", "Mitsubishi"],
+		"model": ["Model:", "Galant"],
+		"year": ["Year:", "1994"],
+		"group": ["Part:", "Engine"],
+		"date": ["Date for repair:", "2012-11-20"],
+		"comments": ["Description:", "exhaust"]
+	},
+	"repair5": {
+		"region": ["Region:", "European"],
+		"make": ["Make:", "Mercedes-Benz"],
+		"model": ["Model:", "S500"],
+		"year": ["Year:", "2003"],
+		"group": ["Part:", "Chassis"],
+		"date": ["Date for repair:", "2012-12-03"],
+		"comments": ["Description:", "tire rotation"]
+	},
+	"repair6": {
+		"region": ["Region:", "Domestic"],
+		"make": ["Make:", "Chevy"],
+		"model": ["Model:", "Silverado 1500"],
+		"year": ["Year:", "2003"],
+		"group": ["Part:", "Body"],
+		"date": ["Date for repair:", "2012-12-15"],
+		"comments": ["Description:", "front passenger fender"]
+	},
+	"repair7": {
+		"region": ["Region:", "Asian"],
+		"make": ["Make:", "Subaru"],
+		"model": ["Model:", "Impreza"],
+		"year": ["Year:", "1996"],
+		"group": ["Part:", "Body"],
+		"date": ["Date for repair:", "2012-12-30"],
+		"comments": ["Description:", "rear driver quarter panel"]
+	},
+	"repair8": {
+		"region": ["Region:", "European"],
+		"make": ["Make:", "BMW"],
+		"model": ["Model:", "Z4"],
+		"year": ["Year:", "1998"],
+		"group": ["Part:", "Chassis"],
+		"date": ["Date for repair:", "2013-1-10"],
+		"comments": ["Description:", "steering rack"]
+	},
+	"repair9": {
+		"region": ["Region:", "Domestic"],
+		"make": ["Make:", "Chrysler"],
+		"model": ["Model:", "Town and Country"],
+		"year": ["Year:", "2000"],
+		"group": ["Part:", "Drivetrain"],
+		"date": ["Date for repair:", "2013-1-18"],
+		"comments": ["Description:", "transmission filter"]
+	},
+	"repair10": {
+		"region": ["Region:", "Asian"],
+		"make": ["Make:", "Hyundai"],
+		"model": ["Model:", "Sonata"],
+		"year": ["Year:", "2006"],
+		"group": ["Part:", "Engine"],
+		"date": ["Date for repair:", "2013-1-27"],
+		"comments": ["Description:", "spark plugs"]
+	},
+	"repair11": {
+		"region": ["Region:", "Domestic"],
+		"make": ["Make:", "Pontiac"],
+		"model": ["Model:", "Trans Am"],
+		"year": ["Year:", "1967"],
+		"group": ["Part:", "Interior"],
+		"date": ["Date for repair:", "2013-2-03"],
+		"comments": ["Description:", "seats"]
+	},
+	"repair12": {
+		"region": ["Region:", "European"],
+		"make": ["Make:", "Rolls Royce"],
+		"model": ["Model:", "Phantom"],
+		"year": ["Year:", "2003"],
+		"group": ["Part:", "Chassis"],
+		"date": ["Date for repair:", "2013-2-12"],
+		"comments": ["Description:", "tire replace"]
+	},"repair13": {
+		"region": ["Region:", "Domestic"],
+		"make": ["Make:", "Chevy"],
+		"model": ["Model:", "Corvette"],
+		"year": ["Year:", "1980"],
+		"group": ["Part:", "Drivetrain"],
+		"date": ["Date for repair:", "2013-2-27"],
+		"comments": ["Description:", "speedometer cable"]
+	},
+	"repair14": {
+		"region": ["Region:", "European"],
+		"make": ["Make:", "Shelby"],
+		"model": ["Model:", "AC Cobra"],
+		"year": ["Year:", "1966"],
+		"group": ["Part:", "Engine"],
+		"date": ["Date for repair:", "2013-3-03"],
+		"comments": ["Description:", "oil change"]
+	},
+	"repair15": {
+		"region": ["Region:", "Asian"],
+		"make": ["Make:", "Lexus"],
+		"model": ["Model:", "LS 400"],
+		"year": ["Year:", "1999"],
+		"group": ["Part:", "Chassis"],
+		"date": ["Date for repair:", "2013-3-14"],
+		"comments": ["Description:", "tie-rod"]
+	},
+	"repair16": {
+		"region": ["Region:", "Domestic"],
+		"make": ["Make:", "Ford"],
+		"model": ["Model:", "Mustang"],
+		"year": ["Year:", "1965"],
+		"group": ["Part:", "Engine"],
+		"date": ["Date for repair:", "2013-3-22"],
+		"comments": ["Description:", "carburator"]
+	},
+	"repair17": {
+		"region": ["Region:", "European"],
+		"make": ["Make:", "Porsche"],
+		"model": ["Model:", "944"],
+		"year": ["Year:", "1987"],
+		"group": ["Part:", "Chassis"],
+		"date": ["Date for repair:", "2013-4-17"],
+		"comments": ["Description:", "rear wheel seals"]
+	},
+	"repair18": {
+		"region": ["Region:", "Asian"],
+		"make": ["Make:", "Nissan"],
+		"model": ["Model:", "Skyline"],
+		"year": ["Year:", "1989"],
+		"group": ["Part:", "Interior"],
+		"date": ["Date for repair:", "2013-4-27"],
+		"comments": ["Description:", "cage"]
+	},
+	"repair19": {
+		"region": ["Region:", "Domestic"],
+		"make": ["Make:", "Dodge"],
+		"model": ["Model:", "Challenger"],
+		"year": ["Year:", "2011"],
+		"group": ["Part:", "Engine"],
+		"date": ["Date for repair:", "2013-5-10"],
+		"comments": ["Description:", "headers"]
+	},
+	"repair20": {
+		"region": ["Region:", "European"],
+		"make": ["Make:", "Land Rover"],
+		"model": ["Model:", "Range Rover"],
+		"year": ["Year:", "1998"],
+		"group": ["Part:", "Chassis"],
+		"date": ["Date for repair:", "2013-1-10"],
+		"comments": ["Description:", "wheel bearings"]
+	},
+}
